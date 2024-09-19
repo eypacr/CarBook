927058bd117c29848f54703bcff3f525b127ed29
Acar <Acar@DESKTOP-DS4AAN1>
2024-09-19 17:18:26 +0300

    About Api

---
+++ Presentation/CarBook.WebApi/Extensions/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using CarBook.Application.Features.CQRS.Handlers.AboutHandlers;
+using CarBook.Application.Interfaces;
+using CarBook.Persistence.Context;
+using CarBook.Persistence.Repositories;
+
+namespace CarBook.WebApi.Extensions
+{
+    public static class ApplicationServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDbContexts(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Burada gerekli connection string veya ayarları kullanarak DbContext'i ekleyebilirsiniz
+            services.AddScoped<CarBookContext>();
+
+            // Diğer DbContext'ler varsa onları da burada ekleyebilirsiniz.
+            // services.AddScoped<AnotherDbContext>();
+
+            return services;
+        }
+        public static IServiceCollection AddRepositoryServices(this IServiceCollection services)
+        {
+            services.AddScoped<CarBookContext>();
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+            return services;
+        }
+
+        public static IServiceCollection AddAboutCQRSHandlers(this IServiceCollection services)
+        {
+            services.AddScoped<GetAboutQueryHandler>();
+            services.AddScoped<GetAboutByIdQueryHandler>();
+            services.AddScoped<CreateAboutCommandHandler>();
+            services.AddScoped<UpdateAboutCommandHandler>();
+            services.AddScoped<RemoveAboutCommandHandler>();
+
+            return services;
+        }
+    }
+}

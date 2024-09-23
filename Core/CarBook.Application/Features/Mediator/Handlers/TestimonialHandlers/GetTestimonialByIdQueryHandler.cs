c0e94db36468ba577f8f8c11d3c6b13adb4c082c
Acar <Acar@DESKTOP-DS4AAN1>
2024-09-23 17:57:29 +0300

    Testimonial Mediator

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/TestimonialHandlers/GetTestimonialByIdQueryHandler.cs
@@ -0,0 +1,64 @@
+﻿using CarBook.Application.Features.Mediator.Queries.TestimonialQueries;
+using CarBook.Application.Features.Mediator.Results.TestimonialResults;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.TestimonialHandlers
+{
+    public class GetTestimonialByIdQueryHandler : IRequestHandler<GetTestimonialByIdQuery, GetTestimonialByIdQueryResult>
+    {
+        private readonly IRepository<Testimonial> _repository;
+
+        public GetTestimonialByIdQueryHandler(IRepository<Testimonial> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<GetTestimonialByIdQueryResult> Handle(GetTestimonialByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByIdAsync(request.Id);
+            return new GetTestimonialByIdQueryResult
+            {
+                TestimonialId = values.TestimonialId,
+                Title = values.Title,
+                Name = values.Name,
+                Comment = values.Comment,
+                ImageUrl = values.ImageUrl
+            };
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

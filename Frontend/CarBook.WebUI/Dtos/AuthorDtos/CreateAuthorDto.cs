aa9f40ef6bc6f9248a3bb9d917a49c313df10cf5
Acar <Acar@ACAR>
2024-12-28 14:53:19 +0300

    Areas Author

---
+++ Frontend/CarBook.WebUI/Dtos/AuthorDtos/CreateAuthorDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CarBook.WebUI.Dtos.AuthorDtos;
+
+public class CreateAuthorDto
+{
+    public string Name { get; set; }
+    public string ImageUrl { get; set; }
+    public string Description { get; set; }
+}
---
+++ Frontend/CarBook.WebUI/Dtos/AuthorDtos/ResultAuthorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CarBook.WebUI.Dtos.AuthorDtos;
+
+public class ResultAuthorDto
+{
+    public int AuthorId { get; set; }
+    public string Name { get; set; }
+    public string ImageUrl { get; set; }
+    public string Description { get; set; }
+}

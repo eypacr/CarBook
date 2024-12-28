0c78a6ff49718c4a267c7e516dcffc2bad449228
Acar <Acar@ACAR>
2024-12-28 13:19:19 +0300

    Areas Banner

---
+++ Frontend/CarBook.WebUI/Dtos/BannerDtos/CreateBannerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CarBook.WebUI.Dtos.BannerDtos;
+
+public class CreateBannerDto
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string VideoDescription { get; set; }
+    public string VideoUrl { get; set; }
+}
---
+++ Frontend/CarBook.WebUI/Dtos/BannerDtos/PlayBannerDto.cs
@@ -0,0 +1,17 @@
+﻿namespace CarBook.WebUI.Dtos.BannerDtos;
+
+public class PlayBannerDto
+{
+    public string VideoUrl { get; set; }
+    public string GetEmbedUrl()
+    {
+        // YouTube URL'sini embed formatına dönüştürme
+        if (!string.IsNullOrEmpty(VideoUrl) && VideoUrl.Contains("youtube.com/watch?v="))
+        {
+            var videoId = VideoUrl.Split(new string[] { "v=" }, StringSplitOptions.None).Last();
+            return $"https://www.youtube.com/embed/{videoId}";
+        }
+
+        return VideoUrl; // Eğer URL başka bir formattaysa, olduğu gibi döndür
+    }
+}

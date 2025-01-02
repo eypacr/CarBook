cddbd5e37955860398c9211e394388189c08f065
Acar <Acar@ACAR>
2025-01-02 14:07:35 +0300

    Areas Contact İşlemleri Yapıldı

---
+++ Frontend/CarBook.WebUI/Dtos/ContactDtos/ContactListDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CarBook.WebUI.Dtos.ContactDtos;
+
+public class ContactListDto
+{
+    public int ContactId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Subject { get; set; }
+    public string Message { get; set; }
+    public DateTime SendingDate { get; set; }
+}

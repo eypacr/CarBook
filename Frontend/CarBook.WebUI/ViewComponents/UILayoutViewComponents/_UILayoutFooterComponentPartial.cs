566a8dfb5d1afa149ca35e40bc5a43e98328f57c
Acar <Acar@ACAR>
2025-01-05 17:26:43 +0300

    Refactor Footer

---
+++ Frontend/CarBook.WebUI/ViewComponents/UILayoutViewComponents/_UILayoutFooterComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using CarBook.WebUI.Dtos.FooterAddressDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.UILayoutViewComponents;
+
+public class _UILayoutFooterComponentPartial : ViewComponent
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _UILayoutFooterComponentPartial(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7204/api/FooterAddresses");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var value = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+            ViewBag.description = value.Select(x => x.Description).FirstOrDefault();
+            return View(value);
+        }
+        return View();
+    }
+}
---
+++ Frontend/CarBook.WebUI/ViewComponents/UILayoutViewComponents/_UILayoutFooterSocialMediaComponentPartial.cs
@@ -0,0 +1,27 @@
+﻿using CarBook.WebUI.Dtos.SocialMediaDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.UILayoutViewComponents;
+
+public class _UILayoutFooterSocialMediaComponentPartial : ViewComponent
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public _UILayoutFooterSocialMediaComponentPartial(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7204/api/SocialMedias");
+        if (responseMessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var value = JsonConvert.DeserializeObject<List<ResultSocialMediaDto>>(jsonData);
+            return View(value);
+        }
+        return View();
+    }
+}

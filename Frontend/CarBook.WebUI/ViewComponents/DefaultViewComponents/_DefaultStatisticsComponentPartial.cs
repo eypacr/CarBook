9d397e53c262a731775c059dbd8bf76613254738
Acar <Acar@ACAR>
2025-01-06 12:43:00 +0300

    Ana sayfadaki istatikler yapıldı.

--- Frontend/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
+++ Frontend/CarBook.WebUI/ViewComponents/DefaultViewComponents/_DefaultStatisticsComponentPartial.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.WebUI.Dtos.StatisticDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
-namespace CarBook.WebUI.ViewComponents.DefaultViewComponents
+namespace CarBook.WebUI.ViewComponents.DefaultViewComponents;
+
+public class _DefaultStatisticsComponentPartial : ViewComponent
 {
-    public class _DefaultStatisticsComponentPartial : ViewComponent
-    {
-        public IViewComponentResult Invoke()
-        {
+    private readonly IHttpClientFactory _httpClientFactory;
 
-            return View();
-        }
+    public _DefaultStatisticsComponentPartial(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responseMessage = await client.GetAsync("https://localhost:7204/api/Statistics");
+        var jsonData = await responseMessage.Content.ReadAsStringAsync();
+        var value = JsonConvert.DeserializeObject<ResultStatisticDto>(jsonData);
+        return View(value);
     }
 }
